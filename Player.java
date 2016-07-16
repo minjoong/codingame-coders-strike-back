ce8e17e7b172feb3aacec56c04329c518a543e2c
Minjoong Kim <minjoong.slow@gmail.com>
2016-07-16 18:02:41 +0900

    Player.java 수정
    디버그 추가 : 1턴 동안 움직인 거리, 현재 위치
    기본 동작 알고리즘 : 다음 체크포인트와 각도가 90도 이상 차이나면 속도 10, 아니면 속도 100
    Boost 동작 알고리즘 : 다음 체크포인트까지 거리 5000 초과, 각도차이 0일 때, Boost 사용

--- Player.java
+++ Player.java
@@ -1,6 +1,7 @@
 import java.util.*;
 import java.io.*;
 import java.math.*;
+import java.lang.Math;
 
 /**
  * Auto-generated code below aims at helping you parse
@@ -10,6 +11,13 @@ class Player {
 
     public static void main(String args[]) {
         Scanner in = new Scanner(System.in);
+        
+        int beforeX = 0;
+        int beforeY = 0;
+        int curX = 0;
+        int curY = 0;
+        boolean isAfterPeak = false;
+        boolean hasBoost = true;
 
         // game loop
         while (true) {
@@ -21,15 +29,47 @@ class Player {
             int nextCheckpointAngle = in.nextInt(); // angle between your pod orientation and the direction of the next checkpoint
             int opponentX = in.nextInt();
             int opponentY = in.nextInt();
+            
+            // default moving value
+            int aimX = nextCheckpointX;
+            int aimY = nextCheckpointY;
+            String thrust = "100";
+            
+            // debug
+            beforeX = curX;
+            beforeY = curY;
+            curX = x;
+            curY = y;
+            double moved = Math.sqrt(Math.pow(curX-beforeX, 2) + Math.pow(curY-beforeY, 2));
+            
+            nextCheckpointAngle = Math.abs(nextCheckpointAngle);
+            
 
             // Write an action using System.out.println()
             // To debug: System.err.println("Debug messages...");
+            System.err.println("Debug messages : x = " + x + ", y = " + y);
+            System.err.println("Debug messages : moved = " + moved);
 
 
             // You have to output the target position
             // followed by the power (0 <= thrust <= 100) or "BOOST" or "SHIELD"
             // i.e.: "x y thrust"
-            System.out.println(nextCheckpointX + " " + nextCheckpointY + " 80");
+            
+            // normal moving
+            if (nextCheckpointAngle > 90) {
+                thrust = "10";
+            } else {
+                thrust = "100";
+            }
+            
+            // a case to boost
+            if (hasBoost && nextCheckpointDist > 5000 && nextCheckpointAngle == 0) {
+                thrust = "BOOST";
+                hasBoost = false;
+            }
+            
+            // move
+            System.out.println(aimX + " " + aimY + " " + " " + thrust);
         }
     }
 }
